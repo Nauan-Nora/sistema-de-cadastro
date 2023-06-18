e76f14424979a1056878c1a94f33e02a3b28fe36
Nauan-Nora <nauangins@gmail.com>
2023-06-18 10:26:52 -0300

    continujação da aula

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 // tipo numero
 
-const meuNumero = 3
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operacaoMatematica)
+
+//pontio flutuande
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteSemZaro = .5;
+
+const novaOperação = numeroPontoFlutuante / numeroPontoFlutuanteSemZaro;
+
+console.log(novaOperação)
