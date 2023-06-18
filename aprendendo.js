be9422566812ec6e9809c3e5d54826df195dc21b
Nauan-Nora <nauangins@gmail.com>
2023-06-18 15:03:50 -0300

    novo arquivo

---
+++ aprendendo.js
@@ -0,0 +1,11 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+let cadastroAtivo = true;
+
+console.log(primeiroNumero === segundoNumero);
+
+const texto1 = 'alura';
+const texto2 = 'a'; 
+
+console.log(texto1 === texto2);
